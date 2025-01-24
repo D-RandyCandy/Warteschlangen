9278415817b3b339ef65d3c995739701ba3b5709
isaacada <danielisaacariwodo9@gmail.com>
2025-01-24 10:41:33 +0100

    commit

--- Kunde.java
+++ Kunde.java
@@ -1,35 +1,18 @@
 public class Kunde {
-    private String name;       // Name des Kunden
-    private Kunde nachfolger;  // nächster Kunden
+    private String name;
 
-    // Standardkonstruktor
     public Kunde() {
-        name = "";
-        nachfolger = null;
+        // Default constructor
     }
 
-    //
     public Kunde(String pName) {
-        name = pName;
-        nachfolger = null;
+        this.name = pName;
     }
 
-    // Setzen des Nachfolgers
-    public void setNachfolger(Kunde pKunde) {
-        nachfolger = pKunde;
-    }
-
-    // Rückgabe des Nachfolgers
-    public Kunde getNachfolger() {
-        return nachfolger;
-    }
-
-    // Setzen des Namens
     public void setName(String pName) {
         name = pName;
     }
 
-    // Rückgabe des Namens
     public String getName() {
         return name;
     }
