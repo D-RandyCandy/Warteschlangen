f23ad03820d93a9d4888e422d98129551aac2d24
isaacada <danielisaacariwodo9@gmail.com>
2025-01-29 11:30:59 +0100

    commit

--- Verwaltung.java
+++ Verwaltung.java
@@ -1,4 +1,22 @@
 public class Verwaltung {
+    public static void main(String[] args){
 
-
+    }
+        public Verwaltung(){
+        scanner = new Scanner(System.in);
+        Warteschlange warteschlange = new Warteschlange();
+        while (true) {
+            System.out.println("==Hauptmenü==");
+            System.out.println("[1]Hinzufügen");
+            System.out.println("[2]Erstes Auslesen");
+            int option = scanner.nextInt();
+            if (option==1) {
+                System.out.println("Wie viele willst du");
+                option = scanner.nextInt();
+                for (int i=0;i<option;i++){
+                    warteschlange.einfuegen(new Kunde(pName""));
+                }else if
+            }
+        }
+        }
 }
