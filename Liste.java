1ee2818005c08688c440b97b2605133a511f92b6
isaacada <danielisaacariwodo9@gmail.com>
2025-02-05 10:37:08 +0100

    commit

--- Liste.java
+++ Liste.java
@@ -1,4 +1,41 @@
+import jdk.jfr.ContentType;
+
 public class Liste <T> {
-    private
+    public KnotenListe <T> Erster;
+    public KnotenListe <T> Letzer;
+    public KnotenListe <T> Aktuelle;
+
+
+
+
+public class Liste <T>   {
+
+}
+
+public void remove () {
+    KnotenListe aktuellerKunde = Erster;
+    if (Erster != null) {
+        Erster = aktuellerKunde.getNachfolger();
+
+    }
+}
+public void next (){
+
+}
+public void append (T pKontent){ KnotenListe ContentType=Erster;
+
+    if (Erster == null){
+        Erster = new KnotenListe<>(pKontent);
+    }
+    else{
+        while(ContentType.getNachfolger() != null) {
+            ContentType = ContentType.getNachfolger();
+        }
+        ContentType.setNachfolger(new KnotenListe(pKontent));
+    }
+
+}
+
+
 
 }
